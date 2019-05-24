f479139879786f4341d68c2737593e24afd8cf45
Delenda <42011189+DelendaJ@users.noreply.github.com>
2019-05-24 17:29:44 -0400

    first commit

---
+++ ArticleApplication/src/main/java/main/MainController.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Finally created by Delenda Joseph on May 22, 2019
+ */
+
+@SpringBootApplication
+public class MainController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class,args);
+    }
+}
